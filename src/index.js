7a9d4e4597e9599896ea22f393c0a382033747ec
pfvo <pedrofvoliveira94@gmail.com>
2022-05-19 15:09:48 +0100

    updateServiceWorker

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import 'tachyons';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 import App from './containers/App';
@@ -25,4 +26,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
